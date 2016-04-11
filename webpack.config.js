0b6f5f1d529acf77ba5533dd9b8db9e3c8ca55cb
jvitus <jeanvitus.albertini@gmail.com>
2016-04-11 12:09:39 +0200

    fichier pour les sources

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/************************************************************/
+/* 															*/
+/*	 fichier de config de webpack							*/
+/*															*/
+/************************************************************/
+
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public'); // dossier source assemble
+var APP_DIR = path.resolve(__dirname, 'src/client/app');      // dossier source du projet react
+
+var config = {
+  entry: APP_DIR + '/index.jsx', //fichier de départ application
+  output: {
+    path: BUILD_DIR,			// chemin de sortie
+    filename: 'bundle.js'		// nom du fichier qui contiendra les sources
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/, //defini les extensions que prend le loader ici js et jsx
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
+  }
+};
+
+module.exports = config;
